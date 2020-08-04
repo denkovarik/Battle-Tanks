c09b6502b78f808abf8bf756e7b22043d8904fed
Dennis <dennis.kovarik@mines.sdsmt.edu>
2020-08-04 16:37:54 -0600

    Added more examples for script to run

--- squad7++/start.sh
+++ squad7++/start.sh
@@ -6,4 +6,23 @@ cp tanks/Edelweiss.so ../projectx/tanks/Edelweiss.so
 
 cd ../projectx
 
+cp ../squad7++/configFiles/mazeWithTarget.txt config.txt
+./platform
+
+cp ../squad7++/configFiles/smallMapWithTargets.txt config.txt
+./platform
+
+cp ../squad7++/configFiles/smallMapMouse.txt config.txt
+./platform
+
+cp ../squad7++/configFiles/smallMapSentry.txt config.txt
+./platform
+
+cp ../squad7++/configFiles/smallMapCat.txt config.txt
+./platform
+
+cp ../squad7++/configFiles/smallMapTurret.txt config.txt
+./platform
+
+cp configFiles/config.txt ../projectx/config.txt
 ./platform
